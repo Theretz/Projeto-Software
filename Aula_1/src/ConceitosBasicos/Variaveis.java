419c094d2bd41e793706643cfb6e3c54def4899e
Vitor Fernandes Acosta Therezo <151594756+Theretz-tikominamata@users.noreply.github.com>
2025-02-10 10:44:34 -0300

    .

---
+++ Aula_1/src/ConceitosBasicos/Variaveis.java
@@ -0,0 +1,11 @@
+package ConceitosBasicos;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola mundo");
+		System.out.print("Hello World");
+
+	}
+
+}
