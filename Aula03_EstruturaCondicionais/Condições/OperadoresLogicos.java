7f49d27f23da53e2b435943d02ffcb1b9e88d082
Theretz <151594756+Theretz@users.noreply.github.com>
2025-02-24 10:46:37 -0300

    Create OperadoresLogicos.java

---
+++ Aula03_EstruturaCondicionais/Condições/OperadoresLogicos.java
@@ -0,0 +1,25 @@
+package Condições;
+
+import java.util.Scanner;
+
+public class OperadoresLogicos {
+	public static void main(String[] args) {
+		//Login e Senha
+			
+		Scanner sc = new Scanner(System.in);
+		
+		String loginSalvo = "vitorfern";
+		String senhaSalva = "1234";
+		
+		String login = sc.next();
+		String senha = sc.next();
+		
+		if (login == loginSalvo && senha == senhaSalva) {
+			System.out.println("Acesso liberado");
+		} else {
+			System.out.println("Acesso negado");
+		}
+		
+		sc.close();
+	}
+}
