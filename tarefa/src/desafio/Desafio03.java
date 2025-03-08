ca9857722fc9a688e8ca71f1403d29f66ec7b4fc
Vitor Fernandes Acosta Therezo <vitortherezo1002@gmail.com>
2025-03-08 02:17:38 -0300

    .

---
+++ tarefa/src/desafio/Desafio03.java
@@ -0,0 +1,44 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Desafio03 {
+	
+	private static float valorTotal;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Taxa de empréstimo 
+		//Peça ao usuário o valor de um empréstimo 
+		//Número de parcelas desejadas (6,12 ou 24 meses)
+		//Estrutura switch para calcular. Considerando as taxas:
+		//6 meses: +5% do valor total
+		//12 meses: +10% do valor total
+		//24 meses: +20% do valor total
+		//Mostre o valor total a ser pago 
+		System.out.println("Valor do empréstimo: ");
+		float valorEmprestimo = sc.nextFloat();
+		
+		System.out.println("Números de parcelas para: "  +valorEmprestimo);
+		System.out.println("1 - 6 meses: +5% do valor total");
+		System.out.println("2 - 12 meses: +10% do valor total");
+		System.out.println("3 - 24 meses: +20% do valor total");
+		int numPar = sc.nextInt();
+		switch(numPar) {
+		case 1:
+			valorTotal = (float) (valorEmprestimo + (valorEmprestimo * 0.005));
+			System.out.println("Este é o valor final: " + valorTotal);
+			break;
+		case 2:
+			valorTotal = (float) (valorEmprestimo + (valorEmprestimo * 0.010));
+			System.out.println("Este é o valor final: " + valorTotal);
+			break;
+		case 3:
+			valorTotal = (float) (valorEmprestimo + (valorEmprestimo * 0.018));
+			break;
+		default:
+			System.out.println("opção inválida");
+		}
+	}
+
+}
---
+++ tarefa/src/desafio/Desafio04.java
@@ -0,0 +1,37 @@
+package desafio;
+
+import java.util.Scanner;
+
+
+public class Desafio04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Seguradora de automóveis 
+		//Peça ao usuário: 
+		//idade
+		//sexo
+		//anos de xp como motorista
+		//M x<25 x <2: 2000 reais
+		//F x<25 x<2: 1800 reais
+		//x>25 or x>2: 1200
+		System.out.println("Idade: ");
+		int idade = sc.nextInt();
+		
+		System.out.println("Sexo: F OU M ");
+		char sexo = sc.next().charAt(0);
+		
+		System.out.println("Anos de experiencia: ");
+		int xp = sc.nextInt();
+		
+		if (sexo == 'M' && idade<=5 && xp<=2) {
+			System.out.println("O seguro custa R$ 2.000 ");
+		} else if (sexo == 'F' && idade<=25 && xp<=2) {
+			System.out.println("O seguro custa R$ 1.800");
+		} else if (idade>=25 || xp>=2) {
+			System.out.println("O seguro custa R$ 1.200");
+		} else {
+			System.out.println("Sinto muito mas não temos nenhum plano de seguro para você");
+		}
+		
+	}
+}
