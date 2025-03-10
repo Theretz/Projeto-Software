f8ae5155c1160a656b6da0d2a763257a6002ad27
Theretz <vitortherezo1002@gmail.com>
2025-03-10 10:49:39 -0300

    .

--- tarefa/src/desafio/LoopFor.java
+++ tarefa/src/desafio/LoopFor.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class LoopFor {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		//Tabuada
-		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Informe um número: ");
 		int num = sc.nextInt();
 		
-		for(int i =1; 1<=10; i++) {
+		for(int i=1; 1<=10; i++) {
 			System.out.println(num + "*" + i + "=" + num*i);
 		}	
+		sc.close();
 		
-		
-		
-	}
-
+	}	
 }
